0926c742be958f205f8389b3eeb60cd8d479c347
Alejandro <aom1008@alu.ubu.es>
2021-04-12 09:57:31 +0200

    Primera version de ActividadSalidaPuerta

--- src/p03/c01/ActividadSalidaPuerta.java
+++ src/p03/c01/ActividadSalidaPuerta.java
@@ -1,8 +1,35 @@
 package src.p03.c01;
 
-public class ActividadSalidaPuerta{
+import java.util.Random;
+
+public class ActividadSalidaPuerta implements Runnable{
 
 	//
 	// TODO
 	//
+	private static final int NUMENTRADAS = 20;
+	private String puerta;
+	private IParque parque;
+	
+	public ActividadSalidaPuerta(String puerta, IParque parque) {
+		this.puerta = puerta;
+		this.parque = parque;
+	}
+
+	@Override
+	public void run() {
+		for (int i = 0; i<NUMENTRADAS; i++) {
+			parque.salirDelParque(puerta);
+			
+			try {
+				
+				Thread.sleep(new Random().nextInt(5) * 1000);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	
 }
