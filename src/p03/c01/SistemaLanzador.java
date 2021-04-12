3ae001bcea1dca7c975c8daec53348f9482e447b
Alejandro <aom1008@alu.ubu.es>
2021-04-12 10:11:09 +0200

    Primera version Sistema Lanzador

--- src/p03/c01/SistemaLanzador.java
+++ src/p03/c01/SistemaLanzador.java
@@ -2,19 +2,21 @@ package src.p03.c01;
 
 public class SistemaLanzador {
 	public static void main(String[] args) {
-		
-		IParque parque = new Parque(); // TODO
+		final int AFORO = 50;
+		IParque parque = new Parque(AFORO); // TODO
 		char letra_puerta = 'A';
 		
 		System.out.println("¡Parque abierto!");
 		
-		for (int i = 0; i < Integer.parseInt(args[0]); i++) {
+		for (int i = 0; i < 5; i++) {
 			
 			String puerta = ""+((char) (letra_puerta++));
 			
-			// Creación de hilos de entrada
+			// Creacion de hilos de entrada
 			ActividadEntradaPuerta entradas = new ActividadEntradaPuerta(puerta, parque);
+			ActividadSalidaPuerta salidas = new ActividadSalidaPuerta(puerta, parque);
 			new Thread (entradas).start();
+			new Thread (salidas).start();
 			
 			// 
 			// TODO
